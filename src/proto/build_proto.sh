d6f32389ae830164e93cf1f5db7b972527f0cecf
mloves0824@163.com <mloves0824>
2018-02-02 02:29:32 -0800

    proto

--- src/proto/build_proto.sh
+++ src/proto/build_proto.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+
 set -x
 
 PROTOC=protoc
 PROTOC_EXTRA_ARGS= 
 
-$(PROTOC) --cpp_out=. --proto_path=. $(PROTOC_EXTRA_ARGS) ./raft.proto
-
-
+$PROTOC --cpp_out=. --proto_path=. $PROTOC_EXTRA_ARGS ./raft.proto
+$PROTOC --cpp_out=. --proto_path=. $PROTOC_EXTRA_ARGS ./raft_kv.proto
