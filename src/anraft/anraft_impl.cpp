47c6a2ab58e94568a9156fd989c7e832b0255397
Ubuntu-dev <mloves0824@163.com>
2018-02-03 00:29:44 +0800

    modify anraft

--- src/anraft/anraft_impl.cpp
+++ src/anraft/anraft_impl.cpp
@@ -1,11 +1,11 @@
 // Copyright (c) 2015 Antalk, Inc.
-//
+// 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+// 
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+// 
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,35 +14,5 @@
 
 // Author: chenbang@antalk.com
 
-#include <string>
-#include "anraft_options.h"
 
-namespace anraft {
 
-enum Status {
-    kOk = 0,
-    kNotFound = 1,
-    kCorruption = 2,
-    kNotSupported = 3,
-    kInvalidArgument = 4,
-    kIOError = 5,
-    kEndFile = 6,
-    kIncomplete = 7,
-    kComplete = 8,
-    kTimeout = 9,
-    kAuthFailed = 10
-};
-
-class AnRaft {
-public:
-	static Status Open(const AnraftOptions &options);
-
-	AnRaft();
-	virtual ~AnRaft();
-
-	virtual Status Put(const std::string &key, const std::string &value);
-	virtual Status Get(const std::string &key, std::string &value);
-
-};
-
-}
--- src/anraft/anraft_options.h
+++ src/anraft/anraft_options.h
@@ -1,11 +1,11 @@
 // Copyright (c) 2015 Antalk, Inc.
-//
+// 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+// 
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+// 
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,36 +13,24 @@
 // limitations under the License.
 
 // Author: chenbang@antalk.com
+#ifndef SOURCE_DIRECTORY__SRC_ANRAFT_ANRAFT_OPTIONS_H_
+#define SOURCE_DIRECTORY__SRC_ANRAFT_ANRAFT_OPTIONS_H_
 
 #include <string>
-#include "anraft_options.h"
 
 namespace anraft {
 
-enum Status {
-    kOk = 0,
-    kNotFound = 1,
-    kCorruption = 2,
-    kNotSupported = 3,
-    kInvalidArgument = 4,
-    kIOError = 5,
-    kEndFile = 6,
-    kIncomplete = 7,
-    kComplete = 8,
-    kTimeout = 9,
-    kAuthFailed = 10
-};
-
-class AnRaft {
+class AnraftOptions {
 public:
-	static Status Open(const AnraftOptions &options);
+	AnraftOptions();
+	~AnraftOptions();
 
-	AnRaft();
-	virtual ~AnRaft();
-
-	virtual Status Put(const std::string &key, const std::string &value);
-	virtual Status Get(const std::string &key, std::string &value);
-
+private:
+	uint64_t check_leader_timer_;
+	uint64_t heatbeat_timer_;
 };
 
 }
+
+
+#endif /* SOURCE_DIRECTORY__SRC_ANRAFT_ANRAFT_OPTIONS_H_ */
