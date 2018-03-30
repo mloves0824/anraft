c7392e9c4f123262db6aba7762d1e346982099a1
mloves0824 <mloves0824@163.com>
2018-03-30 20:43:16 +0800

    modify unstable

--- src/anraft/log_unstable.h
+++ src/anraft/log_unstable.h
@@ -23,6 +23,10 @@
 
 namespace anraft {
 
+// unstable.entries[i] has raft log position i+unstable.offset.
+// Note that unstable.offset may be less than the highest log
+// position in storage; this means that the next write to storage
+// might need to truncate the log before persisting unstable.entries.
 class Unstable {
 public:
     void SetOffset(uint64_t offset) { offset_ = offset; }
